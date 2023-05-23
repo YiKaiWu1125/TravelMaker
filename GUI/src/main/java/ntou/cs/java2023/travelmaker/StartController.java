f38e0293539dca98e053f7e3e34d1132b8ed79d3
fcebk17 <fcebk17@gmail.com>
2023-05-24 01:44:23 +0800

    add: the search page
    + add search-view.fxml
    + modify layout.png

--- GUI/src/main/java/ntou/cs/java2023/travelmaker/StartController.java
+++ GUI/src/main/java/ntou/cs/java2023/travelmaker/StartController.java
@@ -3,12 +3,38 @@ package ntou.cs.java2023.travelmaker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+import java.io.IOException;
 
 public class StartController {
     @FXML
     private AnchorPane anchorPane;
+    @FXML
+    private AnchorPane searchAnchorPane;
 
     @FXML
     private Button startButton;
 
+    @FXML
+    private void clickStartButton(ActionEvent event) {
+        try {
+            Stage currentStage = (Stage) startButton.getScene().getWindow();
+            currentStage.close();
+
+            Stage searchStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("search-view.fxml"));
+            searchStage.setTitle("Go Where?");
+            searchStage.setResizable(false);
+            searchStage.setScene(new Scene(root));
+            searchStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
