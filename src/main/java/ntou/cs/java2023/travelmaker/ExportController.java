b98a2d84aa6910eb8ddf8a6643d62a5832ae9925
fcebk17 <fcebk17@gmail.com>
2023-05-29 05:46:44 +0800

    feat: export-view Page
    + 新增檔案png
    + 修改 export-view layout
    
    完成檔案匯出UI

--- src/main/java/ntou/cs/java2023/travelmaker/ExportController.java
+++ src/main/java/ntou/cs/java2023/travelmaker/ExportController.java
@@ -44,55 +44,55 @@ public class ExportController {
         }
     }
 
-    @FXML
-    private void clickExportFinishedButton(ActionEvent event) {
-        // Create Window Dialog
-        Dialog<ButtonType> dialog = new Dialog<>();
-        dialog.setTitle("完成檔案匯出！");
-        dialog.setHeaderText("真的要離開嗎QAQ");
-
-        DialogPane dialogPane = dialog.getDialogPane();
-
-        // Set dialog css Form
-        dialogPane.getStylesheets().add(getClass().getResource("/static/css/dialog.css").toExternalForm());
-        dialogPane.getStylesheets().add(getClass().getResource("/static/css/button.css").toExternalForm());
-
-        // Create Close Window Button
-        ButtonType closeWindowButtonType = new ButtonType("結束", ButtonBar.ButtonData.CANCEL_CLOSE);
-        dialog.getDialogPane().getButtonTypes().add(closeWindowButtonType);
-        Button closeWindowButton = (Button) dialog.getDialogPane().lookupButton(closeWindowButtonType);
-        closeWindowButton.getStyleClass().add("normalButton"); // Set button styleClass
-
-        // Create Back To search-view Button
-        ButtonType backToSearchButtonType = new ButtonType("回到首頁", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().add(backToSearchButtonType);
-        Button backToSearchButton = (Button) dialog.getDialogPane().lookupButton(backToSearchButtonType);
-        backToSearchButton.getStyleClass().add("normalButton"); // Set button styleClass
-
-        // 監聽按鈕的點擊事件
-        dialog.setOnCloseRequest(event1 -> {
-            ButtonType result = dialog.getResult();
-            if (result == backToSearchButtonType) {
-                // 返回按鈕的事件處理
-                try {
-                    Stage currentStage = (Stage) exportFinishedButton.getScene().getWindow();
-                    currentStage.close();
-
-                    Stage searchStage = new Stage();
-                    Parent root = FXMLLoader.load(getClass().getResource("search-view.fxml"));
-                    searchStage.setTitle("Travel Maker - Go Where?");
-                    searchStage.setResizable(false);
-                    searchStage.setScene(new Scene(root));
-                    searchStage.show();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else if (result == closeWindowButtonType) {
-                // 關閉系統
-                Platform.exit();
-            }
-        });
-        dialog.showAndWait(); // 顯示彈出視窗並等待用戶關閉
-    }
+//    @FXML
+//    private void clickExportFinishedButton(ActionEvent event) {
+//        // Create Window Dialog
+//        Dialog<ButtonType> dialog = new Dialog<>();
+//        dialog.setTitle("完成檔案匯出！");
+//        dialog.setHeaderText("真的要離開嗎QAQ");
+//
+//        DialogPane dialogPane = dialog.getDialogPane();
+//
+//        // Set dialog css Form
+//        dialogPane.getStylesheets().add(getClass().getResource("/static/css/dialog.css").toExternalForm());
+//        dialogPane.getStylesheets().add(getClass().getResource("/static/css/button.css").toExternalForm());
+//
+//        // Create Close Window Button
+//        ButtonType closeWindowButtonType = new ButtonType("結束", ButtonBar.ButtonData.CANCEL_CLOSE);
+//        dialog.getDialogPane().getButtonTypes().add(closeWindowButtonType);
+//        Button closeWindowButton = (Button) dialog.getDialogPane().lookupButton(closeWindowButtonType);
+//        closeWindowButton.getStyleClass().add("normalButton"); // Set button styleClass
+//
+//        // Create Back To search-view Button
+//        ButtonType backToSearchButtonType = new ButtonType("回到首頁", ButtonBar.ButtonData.OK_DONE);
+//        dialog.getDialogPane().getButtonTypes().add(backToSearchButtonType);
+//        Button backToSearchButton = (Button) dialog.getDialogPane().lookupButton(backToSearchButtonType);
+//        backToSearchButton.getStyleClass().add("normalButton"); // Set button styleClass
+//
+//        // 監聽按鈕的點擊事件
+//        dialog.setOnCloseRequest(event1 -> {
+//            ButtonType result = dialog.getResult();
+//            if (result == backToSearchButtonType) {
+//                // 返回按鈕的事件處理
+//                try {
+//                    Stage currentStage = (Stage) exportFinishedButton.getScene().getWindow();
+//                    currentStage.close();
+//
+//                    Stage searchStage = new Stage();
+//                    Parent root = FXMLLoader.load(getClass().getResource("search-view.fxml"));
+//                    searchStage.setTitle("Travel Maker - Go Where?");
+//                    searchStage.setResizable(false);
+//                    searchStage.setScene(new Scene(root));
+//                    searchStage.show();
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            } else if (result == closeWindowButtonType) {
+//                // 關閉系統
+//                Platform.exit();
+//            }
+//        });
+//        dialog.showAndWait(); // 顯示彈出視窗並等待用戶關閉
+//    }
 }
