684553a40dd4e15dfd90be1f0e30dac1afd445db
AnitaX1018 <yujing168100@gmail.com>
2023-05-30 01:06:27 +0800

    fix controller

--- chatBot/src/main/java/com/example/chatbot/ChatGptApplication.java
+++ chatBot/src/main/java/com/example/chatbot/ChatGptApplication.java
@@ -1,7 +1,6 @@
 package com.example.chatbot;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,62 +13,22 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ChatGptApplication extends Application {
-    private ChatGptApi chatApi;
-
-    @FXML
-    private TextArea chatArea;
-    @FXML
-    private TextField inputField;
-    @FXML
-    private Button sendButton;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("chatgpt-view.fxml"));
-        loader.setController(this);
         BorderPane root = loader.load();
 
-        chatArea = (TextArea) loader.getNamespace().get("chatArea");
-        inputField = (TextField) loader.getNamespace().get("inputField");
-        sendButton = (Button) loader.getNamespace().get("sendButton");
+        // 获取从FXML文件中加载的控制器实例
+        ChatgptController controller = loader.getController();
 
-
-        String apiKey = "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";
-        String model = "text-davinci-003";
-        double temperature = 0.4;
-        int maxTokens = 300;
-
-        chatApi = new ChatGptApi(apiKey, model, temperature, maxTokens);
-
-        sendButton.setOnAction(event -> {
-            String message = inputField.getText();
-            if (!message.isEmpty()) {
-                appendToChatArea("你: " + message);
-                String reply = chatApi.getReply(message);
-                appendToChatArea("機器人: " + reply+"\n");
-                inputField.clear();
-            }
-        });
-
-        inputField.setOnAction(event -> {
-            String message = inputField.getText();
-            if (!message.isEmpty()) {
-                appendToChatArea("你: " + message);
-                String reply = chatApi.getReply(message);
-                appendToChatArea("機器人: " + reply + "\n");
-                inputField.clear();
-            }
-        });
+        // 其他代码
 
         Scene scene = new Scene(root, 400, 300);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Chat Room");
         primaryStage.show();
-        inputField.requestFocus();
-    }
-
-    private void appendToChatArea(String message) {
-        chatArea.appendText(message + "\n");
+        controller.getInputField().requestFocus();
     }
 
     public static void main(String[] args) {
--- chatBot/src/main/java/com/example/chatbot/ChatgptController.java
+++ chatBot/src/main/java/com/example/chatbot/ChatgptController.java
@@ -5,9 +5,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import javafx.scene.input.KeyCode;
-
-
 public class ChatgptController {
     @FXML
     private TextArea chatArea;
@@ -21,15 +18,40 @@ public class ChatgptController {
     private ChatGptApi chatApi;
 
     public void initialize() {
-        String apiKey = "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";
+        String apiKey = "KEYYYYYYYYYY，github不給傳keyQQ";
         String model = "text-davinci-003";
         double temperature = 0.4;
         int maxTokens = 300;
 
         chatApi = new ChatGptApi(apiKey, model, temperature, maxTokens);
+
+        sendButton.setOnAction(event -> {
+            String message = inputField.getText();
+            if (!message.isEmpty()) {
+                appendToChatArea("你: " + message);
+                String reply = chatApi.getReply(message);
+                appendToChatArea("機器人: " + reply+"\n");
+                inputField.clear();
+            }
+        });
+
+        inputField.setOnAction(event -> {
+            String message = inputField.getText();
+            if (!message.isEmpty()) {
+                appendToChatArea("你: " + message);
+                String reply = chatApi.getReply(message);
+                appendToChatArea("機器人: " + reply + "\n");
+                inputField.clear();
+            }
+        });
     }
 
     private void appendToChatArea(String message) {
         chatArea.appendText(message + "\n");
+    }
+
+    // 新增的方法用于返回输入文本框的引用
+    public TextField getInputField() {
+        return inputField;
     }
 }
